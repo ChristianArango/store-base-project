bebc985637f06fe214b6d80a8567e27733d641b1
ChristianArango <christian.arango.20@outlook.com>
2025-10-19 15:36:44 -0500

    Nueva versión de frontend

--- store-frontend-base/index.js
+++ store-frontend-base/index.js
@@ -1,6 +1,9 @@
+const productosRuta = "http://localhost:3000/products/"
+const formularioProducto = document.getElementById("add-product-form")
+
 const contenedor = document.getElementById("productos-container");
 
-  fetch("http://localhost:3000/products")
+fetch(productosRuta)
     .then((res) => res.json())
     .then((data) => {
       data.forEach((producto) => {
@@ -20,3 +23,45 @@ const contenedor = document.getElementById("productos-container");
       contenedor.innerHTML = "<p>Error al cargar productos.</p>";
       console.error("Error:", error);
     });
+
+
+formularioProducto.addEventListener('submit', (e) => { 
+    e.preventDefault();
+
+    const name = e.target.elements['product-name'].value;
+    const price = parseFloat(e.target.elements['product-price'].value);
+    const description = e.target.elements['product-description'].value;
+    const stock = e.target.elements['product-stock'].value;
+    const sku = e.target.elements['product-sku'].value;
+
+    const datosEnviar = {
+        name: name,
+        price: price,
+        description: description,
+        stock: stock,
+        sku: sku
+    };
+
+    console.log(datosEnviar);
+
+    fetch(productosRuta, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            datosEnviar
+        )
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        alert("Producto agregado exitosamente");
+        formularioProducto.reset();
+        location.reload();
+    })
+    .catch((error) => {
+      contenedor.innerHTML = "<p>Error al agregar el producto.</p>";
+      console.error("Error:", error);
+    });
+
+})
